093fb9b43659b41465d222ccd5c99332a7d80d16
Dright <as58140399@gmail.com>
2013-08-23 18:35:09 +0800

    move sql to controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :price
-  validates :name, :uniqueness => true
+  validates :name, :uniqueness => true, :presence => true
+  validates :price, :presence => true
 end
